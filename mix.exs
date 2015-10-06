f1d7a9ab955cbe9692ccb21eb9c33dc698cb3734
Ile Eftimov <ileeftimov@gmail.com>
2015-10-06 20:57:57 +0200

    depends on HTTPoison

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Excountries.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,6 @@ defmodule Excountries.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [{:httpoison, "~> 0.7.2"}]
   end
 end
