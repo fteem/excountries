02cd93596c7de6716a83a7623d5917f53c273911
Pedro Vieira <pedro.vieira@phishx.io>
2017-10-23 11:14:16 -0200

    support_to_api_v2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Excountries.Mixfile do
 
   def project do
     [app: :excountries,
-     version: "0.0.3",
+     version: "0.0.4",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -12,22 +12,10 @@ defmodule Excountries.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger, :httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
       {:httpoison, "~> 0.7.2"},
--- test/excountries/radar_test.exs
+++ test/excountries/radar_test.exs
@@ -1,99 +1,49 @@
 defmodule Excountries.RadarTest do
   use ExUnit.Case
 
-  defmodule ClientMock do
-    def call(_path) do
-      ~s(
-      [
-        {
-          "name": "Republic of Macedonia",
-          "capital": "Skopje",
-          "altSpellings": [
-            "MK",
-            "Republic of Macedonia",
-            "Република Македонија"
-          ],
-          "relevance": "0",
-          "region": "Europe",
-          "subregion": "Southern Europe",
-          "translations": {
-            "de": "Mazedonien",
-            "es": "Macedonia",
-            "fr": "Macédoine",
-            "ja": "マケドニア旧ユーゴスラビア共和国",
-            "it": "Macedonia"
-          },
-          "population": 2058539,
-          "latlng": [
-            41.83333333,
-            22
-          ],
-          "demonym": "Macedonian",
-          "area": 25713,
-          "gini": 43.2,
-          "timezones": [
-            "UTC+01:00"
-          ],
-          "borders": [
-            "ALB",
-            "BGR",
-            "GRC",
-            "KOS",
-            "SRB"
-          ],
-          "nativeName": "Македонија",
-          "callingCodes": [
-            "389"
-          ],
-          "topLevelDomain": [
-            ".mk"
-          ],
-          "alpha2Code": "MK",
-          "alpha3Code": "MKD",
-          "currencies": [
-            "MKD"
-          ],
-          "languages": [
-            "mk"
-          ]
-        }
-      ]
-      )
-    end
-  end
-
   test ".all returns a list of Country structs" do
-    countries = Excountries.Radar.all(ClientMock)
-    country = List.first countries
-    assert country.name, "Republic Of Macedonia" 
-    assert country.capital, "Skopje"
-    assert country.region, "Europe"
-    assert country.relevance, "0"
+    countries =
+      Excountries.Radar.all("alpha2Code;capital")
+      |> Enum.filter(fn x -> x.alpha2Code == "MK" end)
+      |> List.first()
+    assert countries.capital, "Skopje"
   end
 
   test ".by_full_name returns the matching Country" do
-    country = Excountries.Radar.by_full_name("Republic Of Macedonia", ClientMock)
-    assert country.name, "Republic Of Macedonia" 
-    assert country.capital, "Skopje" 
+    country = Excountries.Radar.by_full_name("Spain")
+    assert country.capital, "Madrid"
   end
 
   test ".by_language returns the countries where the language is spoken" do
-    countries = Excountries.Radar.by_language("mk", ClientMock)
+    countries = Excountries.Radar.by_language("mk")
     country = List.first countries
-    assert country.name, "Republic Of Macedonia" 
-    assert List.first(country.languages), "mk"
+    assert country.name, "Macedonia (the former Yugoslav Republic of)"
   end
 
   test ".by_currency returns the countries where the currency is used" do
-    countries = Excountries.Radar.by_currency("MKD", ClientMock)
+    countries = Excountries.Radar.by_currency("MKD")
     country = List.first countries
-    assert country.name, "Republic Of Macedonia" 
-    assert List.first(country.currencies), "MKD"
+    assert country.name, "Macedonia (the former Yugoslav Republic of)"
   end
 
   test ".by_capital returns the country whose capital matches the name" do
-    country = Excountries.Radar.by_capital("Skopje", ClientMock)
-    assert country.name, "Republic Of Macedonia" 
-    assert country.capital, "Skopje"
+    country = Excountries.Radar.by_capital("Skopje")
+    assert country.name, "Macedonia (the former Yugoslav Republic of)"
+  end
+
+  test ".by_regional_bloc returns the country whose regional bloc matches the name" do
+    country =
+      Excountries.Radar.by_regional_bloc("eu")
+      |> Enum.filter(fn x -> x.alpha2Code == "ES" end)
+      |> List.first()
+    assert country.name, "Spain"
+  end
+
+  test "with filter returns the country fields whose regional bloc matches the name" do
+    country =
+      Excountries.Radar.by_regional_bloc("eu", "name;alpha2Code;capital")
+      |> Enum.filter(fn x -> x.alpha2Code == "ES" end)
+      |> List.first()
+    assert country.name, "Spain"
   end
 end
